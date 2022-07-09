bed556914a7096d38aa505d5e394e2e74f8f2995
Prashant Srivastava <prashantsrivastava381@gmail.com>
2022-07-10 01:42:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
+
+//get local starage
+const getLocalData = () => {
+  let data= localStorage.getItem('goal');
+  if(data){
+    return JSON.parse(localStorage.getItem('goal'));
+  }else{
+    return [
+      { text: 'Do all exercises!', id: 'g1' },
+      { text: 'Finish the course!', id: 'g2' }
+    ];
+  }
+}
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState(getLocalData());
+
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No data found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
+  //save to local storage
+  useEffect(() => {
+    localStorage.setItem('goal', JSON.stringify(courseGoals));
+  }, [courseGoals])
+  
+
+  return (
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+        {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
+    </div>
+  );
+};
+
+export default App;
