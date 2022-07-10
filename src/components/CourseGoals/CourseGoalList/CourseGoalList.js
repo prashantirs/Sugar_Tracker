5f62d7b2776d79afaedffcdceb8139fc806b949e
Prashant Srivastava <prashantsrivastava381@gmail.com>
2022-07-10 16:15:06 +0530

    Delete Icon added

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
 import './CourseGoalList.css';
-
+import 'font-awesome/css/font-awesome.min.css';
 const CourseGoalList = props => {
   return (
     <ul className="goal-list">
-      {props.items.map(goal => (
+      {props.items.map(goal => ( 
         <CourseGoalItem
           key={goal.id}
           id={goal.id}
-          onDelete={props.onDeleteItem}
-        >
+          onDelete={props.onDeleteItem} 
+        > 
+            <div>
           {goal.text}
+          <i className="icon fas fa-trash "/>
+          </div>
         </CourseGoalItem>
       ))}
     </ul>
